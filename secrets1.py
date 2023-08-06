f2b260e66379cf40c6a6d8b9f3ff7d0992f84bc6
galdavidi <gal@cycode.com>
2023-08-06 17:41:23 +0300

    sdfghk1

---
+++ secrets1.py
@@ -0,0 +1,37 @@
+
+
+amazon="amzn.mws.00a19111-8311-5311-5391-5a211cbfcd6d"
+
+google="AIzaSyDBNvw7c_w3RdC7ghMGp_XfTWGZdsfAkUA"
+
+
+
+ 12312313213213
+  asdda 
+secrets:
+
+
+amazon: amzn.mws.00a19111-8311-5311-5391-5a211cbfcd6d
+
+google: AIzaSyDBNvw7c_w3RdC7ghMGp_XfTWGZdsfAkUA
+google: AIzaSyDBNvw7c_w3RdC7ghMGp_XfTWGZdsfAkUA
+
+
+1231
+..
+..
+..
+secrets:
+
+
+amazon: amzn.mws.00a19111-8311-5311-5391-5a211cbfcd6d
+
+google: AIzaSyDBNvw7c_w3RdC7ghMGp_XfTWGZdsfAkUA
+google: AIzaSyDBNvw7c_w3RdC7ghMGp_XfTWGZdsfAkUA
+
+
+1231
+..
+..
+..
+1
